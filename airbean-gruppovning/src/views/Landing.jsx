4dc36ff2a32ec67eb77d27de9317cb29ece75851
Jamildanielsson <114480495+Jamildanielsson@users.noreply.github.com>
2023-03-27 09:52:34 +0200

    Landing-page, Nav-page CSS only

--- airbean-gruppovning/src/views/Landing.jsx
+++ airbean-gruppovning/src/views/Landing.jsx
@@ -5,6 +5,17 @@ function Landing() {
     return (
         <>
 
+        <article className='landing-article'>
+
+        <img src="./src/assets/graphics/intro-graphic-left.svg" alt="" />
+
+
+        <div className='landing-logo'> <img src="./src/assets/graphics/airbean-landing.svg" alt="" /> </div>
+
+
+        <img src="./src/assets/graphics/intro-graphic-right.svg" alt="" />
+
+        </article>
 
         </>
   )
--- airbean-gruppovning/src/views/Nav.jsx
+++ airbean-gruppovning/src/views/Nav.jsx
@@ -4,7 +4,25 @@ function Nav() {
   
     return (
         <>
+        <article className='nav-article'>
+        <header className='nav-header'> 
+        <div className='roundbox'><img className='cross' src="./src/assets/graphics/close.svg" alt="cross" /></div>
+        </header>
 
+        <section className='nav-section'>
+
+        <ul>
+          <li>Meny</li>
+          <hr />
+          <li>Vårt Kaffe</li>
+          <hr />
+          <li>Orderstatus</li>
+          <hr />
+        </ul>
+
+        </section>
+
+        </article>
         </>
   )
 }
