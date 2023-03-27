7c76d5822f1ff735374ef5c7b00acac6d9eaff3c
Josefine Forsman <josefineforsman1@gmail.com>
2023-03-27 14:02:49 +0200

    navigate to views

--- airbean-gruppovning/src/views/Landing/Landing.jsx
+++ airbean-gruppovning/src/views/Landing/Landing.jsx
@@ -1,12 +1,18 @@
 import './Landing.css'
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function Landing() {
+  const navigate = useNavigate();
+
+  function goToMenu(){
+    navigate('/menu');
+  }
   
     return (
         <>
 
-        <article className='landing-article'>
+        <article className='landing-article' onClick={ goToMenu }>
 
         <img src="./src/assets/graphics/intro-graphic-left.svg" alt="" />
 
