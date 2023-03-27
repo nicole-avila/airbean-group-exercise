e21a59a34e8d53dd3943e209746ff866903041df
MartinaSTV <foldermate@hotmail.com>
2023-03-27 14:06:54 +0200

    menu bild

--- airbean-gruppovning/src/components/menuComponent/MenuComponent.jsx
+++ airbean-gruppovning/src/components/menuComponent/MenuComponent.jsx
@@ -1,14 +1,16 @@
 import './menuComponent.css'
+import Addbutton from '../../assets/graphics/add.svg'
 function MenuComponent(props){
 
     console.log(props.item)
 
     return(
         <section className="menu">
-            <button>+</button>
+            
             <section className="menu__text">
                 
                 <section className='menu_titles'>
+                    <article> <img src={Addbutton} alt="add"  className='menu__img'/> </article>
                     <h1>{props.item.title}</h1>
                     <p>{props.item.price}</p>
                 </section>
