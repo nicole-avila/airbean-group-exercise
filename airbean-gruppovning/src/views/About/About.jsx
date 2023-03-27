d700c9aebd6c45c3394c7c76aaffc75084015948
Jamildanielsson <114480495+Jamildanielsson@users.noreply.github.com>
2023-03-27 13:53:20 +0200

    header + about

--- airbean-gruppovning/src/views/About/About.jsx
+++ airbean-gruppovning/src/views/About/About.jsx
@@ -1,14 +1,13 @@
 import './About.css'
 import React from 'react'
+import Header from '../../components/header/Header';
 
 function About() {
   
     return (
         <>
         <article className='about-article'>
-        <header className='about-header'>
-        <div className='about-roundbox'> <img src="../../../src/assets/graphics/navicon.svg" alt="" /></div>
-        </header>
+        <Header />
 
 
 
