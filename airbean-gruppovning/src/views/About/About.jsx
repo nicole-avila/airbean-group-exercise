f3e8045a1a0a6b260fbd872d83f95c6c88db2fc9
Jamildanielsson <114480495+Jamildanielsson@users.noreply.github.com>
2023-03-27 13:46:04 +0200

    Börjat med About

--- airbean-gruppovning/src/views/About/About.jsx
+++ airbean-gruppovning/src/views/About/About.jsx
@@ -5,9 +5,19 @@ function About() {
   
     return (
         <>
+        <article className='about-article'>
+        <header className='about-header'>
+        <div className='about-roundbox'> <img src="../../../src/assets/graphics/navicon.svg" alt="" /></div>
+        </header>
 
+
+
+
+
+
+        </article>
         </>
   )
-}
+} 
 
 export default About;
