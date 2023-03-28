4710447f302a84609fc81e3faacac413d89dcfd6
MartinaSTV <foldermate@hotmail.com>
2023-03-28 11:42:34 +0200

    iconCart

--- airbean-gruppovning/src/components/header/Header.jsx
+++ airbean-gruppovning/src/components/header/Header.jsx
@@ -1,12 +1,17 @@
 import './Header.css'
 import React from 'react'
+import IconCart from '../IkonCart/IkonCart';
 
 function Header() {
   return (
-        <>
-
+        <> 
+        
         <header className='main-header'>
         <div className='roundbox'> <img src="../../../src/assets/graphics/navicon.svg" alt="" /></div>
+          <article className='iconCart'>
+            <IconCart/>
+          </article>
+        
         </header>
         </>
   )
