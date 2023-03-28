7a1641266c8f84b2a571d12217bdbf52c061a83c
Jamildanielsson <114480495+Jamildanielsson@users.noreply.github.com>
2023-03-28 10:41:37 +0200

    Updatering nav-knapp header

--- airbean-gruppovning/src/components/header/Header.jsx
+++ airbean-gruppovning/src/components/header/Header.jsx
@@ -1,12 +1,23 @@
 import './Header.css'
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
 
 function Header() {
+
+const navigate = useNavigate()
+
+function openNav(){
+
+  navigate('/nav')
+
+}
+
   return (
         <>
 
         <header className='main-header'>
-        <div className='roundbox'> <img src="../../../src/assets/graphics/navicon.svg" alt="" /></div>
+        <div className='roundbox' onClick={ openNav }> <img src="../../../src/assets/graphics/navicon.svg" alt="" /></div>
         </header>
         </>
   )
