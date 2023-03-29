b349099c08eb197b5cae6dcf8ea94993c190097d
Josefine Forsman <josefineforsman1@gmail.com>
2023-03-29 12:16:17 +0200

    status minutes update done

--- airbean-gruppovning/src/views/Status/Status.jsx
+++ airbean-gruppovning/src/views/Status/Status.jsx
@@ -3,18 +3,31 @@ import './Status.css'
 import drone from '../../assets/graphics/drone.svg'
 import { useNavigate } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 
 function Status() {
- 
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  const location = useLocation()
-  let ETA = location.state.orderNumber.eta
+  //let ETA = location.state.orderNumber.eta
   let orderId = location.state.orderNumber.orderNr
-  console.log(ETA)
+  let [time, setTime] = useState();
+
+  //console.log(ETA)
   console.log(orderId)
 
-  const navigate = useNavigate();
+  useEffect(()=>{
+    async function getTime(){
+      const response = await fetch(`https://airbean.awesomo.dev/api/beans/order/status/${orderId}`);
+      const data = await response.json()
+     
+      setTime(data.eta)
+      console.log(data.eta)
+    }
+    getTime();
+  },[])
+
 
   function goToLanding() {
     navigate('/')
@@ -26,7 +39,7 @@ function Status() {
               <p className='status__text'>Ordernummer: {orderId}</p>
               <img className='status__img' src= {drone} alt="image of a black color drone holding a white cup" />
               <h3 className='status__title'>Din beställning är på väg!</h3>
-              <p>{} minuter</p>
+              <p>{time} minuter</p>
 
 
               <button className='button button__status'onClick = {goToLanding} >ok, cool!</button>
